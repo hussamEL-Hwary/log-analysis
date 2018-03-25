a8c42916175113a6133059fd20164dd336475553
vagrant <vagrant@vagrant.vm>
2018-03-25 23:43:55 +0000

    feat: most popular article authors

--- analyze_logs.py
+++ analyze_logs.py
@@ -16,4 +16,16 @@ rows = cur.fetchall()
 print "\t most popular three articles"
 for row in rows:
     print "%s \t %s views" %(row[0], row[1])
+#cur.close()
+
+# most popular article authors
+cur.execute("select authors.name, top_writers.sum "
+            "from authors, top_writers "
+            "where authors.id = top_writers.author "
+            "order by top_writers.sum DESC")
+
+rows = cur.fetchall()
+print "\n\t most popular article authors"
+for row in rows:
+    print "%s \t %s views" %(row[0], row[1])
 cur.close()
